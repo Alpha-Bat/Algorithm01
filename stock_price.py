b4820d747c53cf5f6b549715788132bb985641ce
Lee Dong-woo <57128307+Alpha-Bat@users.noreply.github.com>
2022-06-02 19:46:06 +0900

    Update stock_price.py

--- stock_price.py
+++ stock_price.py
@@ -1,13 +1,12 @@
 def solution(prices):
     answer = []
-    prices.pop()
     for idx, price in enumerate(prices) :
-        n = 1
-        for i in range(idx+1, len(prices)) :
-            if price <= prices[i] :
+        n = 0
+        for i in range(idx+1, len(prices)) : # 현재 price의 다음 인덱스부터 조사
+            if price <= prices[i] : # price보다 가격이 떨어지지 않았다면
                 n += 1
             else :
+                n += 1 # 가격이 떨어지는 1초간에도 가격이 떨이지지 않은것으로 보기때문에 한번더 더해준다.
                 break
-        answer.append(n)
-    answer.append(0)
+        answer.append(n) # n이 idx 인덱스의 price의 가격이 떨어지지 않은 기간이므로 결과값에 append해준다.
     return answer
